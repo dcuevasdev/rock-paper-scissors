880978356f231bf160d2226b761318e646b1d0f9
dcuevas24 <ivandaniel1224@gmail.com>
2021-10-05 17:19:21 -0500

    Interaccion con el usuario

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,39 @@
 const options = ["Rock", "Paper", "Scissors"];
-const optionHouse = Math.floor(Math.random() * options.length);
-const optionPlayer = Math.floor(Math.random() * options.length);
+let optionPlayer;
 
-if (
-  (options[optionHouse] == "Rock" && options[optionPlayer] == "Paper") ||
-  (options[optionHouse] == "Paper" && options[optionPlayer] == "Scissors") ||
-  (options[optionHouse] == "Scissors" && options[optionPlayer] == "Rock")
-) {
-  console.log(
-    `Player: ${options[optionPlayer]} VS House: ${options[optionHouse]} <br/> Win Player`
-  );
-} else if (
-  (options[optionHouse] == "Paper" && options[optionPlayer] == "Rock") ||
-  (options[optionHouse] == "Scissors" && options[optionPlayer] == "Paper") ||
-  (options[optionHouse] == "Rock" && options[optionPlayer] == "Scissors")
-) {
-  console.log(
-    `Player: ${options[optionPlayer]} VS House: ${options[optionHouse]} <br/> Win House`
-  );
-} else {
-  console.log(
-    `Player: ${options[optionPlayer]} VS House: ${options[optionHouse]} <br/> It's a tie`
-  );
+const randomHouse = () => {
+  return Math.floor(Math.random() * options.length);
+};
+
+function onclickScissors() {
+  const valueScissors = document.getElementById("buttonScissors");
+  optionPlayer = valueScissors.value;
+
+  const optionHouse = randomHouse();
+
+  game(optionHouse, optionPlayer);
+}
+
+function game(optionHouse, optionPlayer) {
+  if (
+    (options[optionHouse] == "Rock" && optionPlayer == "Paper") ||
+    (options[optionHouse] == "Paper" && optionPlayer == "Scissors") ||
+    (options[optionHouse] == "Scissors" && optionPlayer == "Rock")
+  ) {
+    document.write(
+      `Player: ${optionPlayer} VS House: ${options[optionHouse]} <strong> Win Player </strong> `
+    );
+  } else if (
+    (options[optionHouse] == "Paper" && optionPlayer == "Rock") ||
+    (options[optionHouse] == "Scissors" && optionPlayer == "Paper") ||
+    (options[optionHouse] == "Rock" && optionPlayer == "Scissors")
+  ) {
+    document.write(
+      `Player: ${optionPlayer} VS House: ${options[optionHouse]} Win House`
+    );
+  } else {
+    document.write(
+      `Player: ${optionPlayer} VS House: ${options[optionHouse]} It's a tie`
+    );
+  }
 }
